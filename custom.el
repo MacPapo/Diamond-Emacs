2a011ed638d9565c45fcd8375bf1d43a738a3f4d
MacPapo <jacopocostantini32@gmail.com>
2023-01-25 16:01:07 +0100

    Diamond Emacs Reborn
    Diamond Emacs is back!
    
    Minimal Approch, ivy, counsel and swiper based completion.
    
    Eglot and corfu and some other tweaks.

---
+++ custom.el
@@ -0,0 +1,17 @@
+;; -*- lexical-binding: t; -*-
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doc-view-continuous t)
+ '(package-selected-packages
+   '(winum which-key dashboard diminish counsel consuel swiper ivy beacon corfu auto-package-update rg exec-path-from-shell org-pomodoro)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
