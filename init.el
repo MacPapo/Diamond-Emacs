8a4e0b078d4507858d64ca6457eda3b265025d80
MacPapo <jacopocostantini32@gmail.com>
2023-02-17 11:15:14 +0100

    Elgot as CAPF

--- init.el
+++ init.el
@@ -317,30 +317,225 @@
 (use-package yasnippet-snippets
   :after yasnippet)
 
-(use-package company
-  :diminish company-mode
+;; (use-package company
+;;   :diminish company-mode
+;;   :custom
+;;   (company-idle-delay 0)
+;;   (company-minimum-prefix-length 1)
+;;   (company-selection-wrap-around t)
+;;   :config
+
+;;   (defun my-text-mode ()
+;;     (setq-local company-backends
+;; 		'((company-dabbrev company-ispell :separate)
+;;                   company-files)))
+
+;;   (defun my-prog-mode ()
+;;     (setq-local company-backends
+;; 		'((company-capf :with company-yasnippet)
+;;                   (company-capf company-dabbrev-code))))
+
+;;   (setq company-backends '((company-capf :with company-yasnippet)))
+;;   :hook
+;;   ((text-mode . my-text-mode))
+;;   ((prog-mode . my-prog-mode))
+;;   :init
+;;   (global-company-mode))
+
+(use-package corfu
   :custom
-  (company-idle-delay 0)
-  (company-minimum-prefix-length 1)
-  (company-selection-wrap-around t)
-  :config
+  (corfu-cycle t)
+  (corfu-auto t)
+  (corfu-auto-prefix 1)
+  (corfu-auto-delay 0.0)
+  (corfu-quit-no-match t)
+  (corfu-preview-current 'insert)
+  (corfu-preselect 'prompt)
 
-  (defun my-text-mode ()
-    (setq-local company-backends
-		'((company-dabbrev company-ispell :separate)
-                  company-files)))
+  (lsp-completion-provider :none) ; Use corfu instead for lsp completions
 
-  (defun my-prog-mode ()
-    (setq-local company-backends
-		'((company-capf :with company-yasnippet)
-                  (company-capf company-dabbrev-code))))
-
-  (setq company-backends '((company-capf :with company-yasnippet)))
-  :hook
-  ((text-mode . my-text-mode))
-  ((prog-mode . my-prog-mode))
+  ;; Works with `indent-for-tab-command'. Make sure tab doesn't indent when you
+  ;; want to perform completion
+  (tab-always-indent 'complete)
+  (completion-cycle-threshold nil)    ; Always show candidates in menu
+  
+  :bind
+  (:map corfu-map
+	("C-n"		.	'corfu-next)
+	("C-p"		.	'corfu-previous)
+	("<escape>"	.	'corfu-quit)
+	("<return>"	.	'corfu-insert)
+	("S-SPC"	.	'corfu-insert-separator))
   :init
-  (global-company-mode))
+  (global-corfu-mode)
+  (corfu-history-mode)
+  :config
+  (add-hook 'eshell-mode-hook
+	    (lambda () (setq-local corfu-quit-at-boundary t
+				   corfu-quit-no-match t
+				   corfu-auto nil)
+	      (corfu-mode)))
+  ;; Enable Corfu more generally for every minibuffer, as long as no other
+  ;; completion UI is active. If you use Mct or Vertico as your main minibuffer
+  ;; completion UI. From
+  ;; https://github.com/minad/corfu#completing-with-corfu-in-the-minibuffer
+  (defun corfu-enable-always-in-minibuffer ()
+    "Enable Corfu in the minibuffer if Vertico/Mct are not active."
+    (unless (or (bound-and-true-p mct--active) ; Useful if I ever use MCT
+                (bound-and-true-p vertico--input))
+      (setq-local corfu-auto nil) ; Ensure auto completion is disabled
+      (corfu-mode 1)))
+  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1)
+  
+  ;; Setup lsp to use corfu for lsp completion
+  (defun kb/corfu-setup-lsp ()
+    "Use orderless completion style with lsp-capf instead of the default lsp-passthrough."
+    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+          '(orderless))))
+
+(use-package kind-icon
+  :after corfu
+  :custom
+  (kind-icon-use-icons t)
+  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
+  (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
+  (kind-icon-blend-frac 0.08)
+
+  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
+  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
+  ;; directory to a location appropriate to `no-littering' conventions, a
+  ;; package which moves directories of other packages to sane locations.
+  ;; (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
+
+  ;; Add hook to reset cache so the icon colors match my theme
+  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
+  ;; the theme using my custom defined command for switching themes. If I don't
+  ;; do this, then the backgound color will remain the same, meaning it will not
+  ;; match the background color corresponding to the current theme. Important
+  ;; since I have a light theme and dark theme I switch between. This has no
+  ;; function unless you use something similar
+  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
+
+(use-package cape
+  :hook (
+	 (emacs-lisp-mode .  kb/cape-capf-setup-elisp)
+         (lsp-completion-mode . kb/cape-capf-setup-lsp)
+         (org-mode . kb/cape-capf-setup-org)
+         (eshell-mode . kb/cape-capf-setup-eshell)
+         (LaTeX-mode . kb/cape-capf-setup-latex)
+         (sh-mode . kb/cape-capf-setup-sh)
+         )
+  ;; :general (:prefix "M-c"               ; Particular completion function
+  ;;           "p" 'completion-at-point
+  ;;           "t" 'complete-tag           ; etags
+  ;;           "d" 'cape-dabbrev           ; or dabbrev-completion
+  ;;           "f" 'cape-file
+  ;;           "k" 'cape-keyword
+  ;;           "s" 'cape-symbol
+  ;;           "a" 'cape-abbrev
+  ;;           "i" 'cape-ispell
+  ;;           "l" 'cape-line
+  ;;           "w" 'cape-dict
+  ;;           "\\"' cape-tex
+  ;;           "_" 'cape-tex
+  ;;           "^" 'cape-tex
+  ;;           "&" 'cape-sgml
+  ;;           "r" 'cape-rfc1345
+  ;;           )
+  :custom
+  (cape-dabbrev-min-length 3)
+  :init
+  
+  ;; Elisp
+  (defun kb/cape-capf-ignore-keywords-elisp (cand)
+    "Ignore keywords with forms that begin with \":\" (e.g.
+:history)."
+    (or (not (keywordp cand))
+        (eq (char-after (car completion-in-region--data)) ?:)))
+  (defun kb/cape-capf-setup-elisp ()
+    "Replace the default `elisp-completion-at-point'
+completion-at-point-function. Doing it this way will prevent
+disrupting the addition of other capfs (e.g. merely setting the
+variable entirely, or adding to list).
+
+Additionally, add `cape-file' as early as possible to the list."
+    (setf (elt (cl-member 'elisp-completion-at-point completion-at-point-functions) 0)
+          #'elisp-completion-at-point)
+    (add-to-list 'completion-at-point-functions #'cape-symbol)
+    ;; I prefer this being early/first in the list
+    (add-to-list 'completion-at-point-functions #'cape-file)
+    (require 'company-yasnippet)
+    (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-yasnippet)))
+
+  ;; LSP
+  (defun kb/cape-capf-setup-lsp ()
+    "Replace the default `lsp-completion-at-point' with its
+`cape-capf-buster' version. Also add `cape-file' and
+`company-yasnippet' backends."
+    (setf (elt (cl-member 'lsp-completion-at-point completion-at-point-functions) 0)
+          (cape-capf-buster #'lsp-completion-at-point))
+    ;; TODO 2022-02-28: Maybe use `cape-wrap-predicate' to have candidates
+    ;; listed when I want?
+    (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-yasnippet))
+    (add-to-list 'completion-at-point-functions #'cape-dabbrev t))
+
+  ;; Org
+  (defun kb/cape-capf-setup-org ()
+    ;; (require 'org-roam)
+    ;; (if (org-roam-file-p)
+    ;;     (org-roam--register-completion-functions-h)
+    (let (result)
+      (dolist (element (list
+                        (cape-super-capf #'cape-ispell #'cape-dabbrev)
+                        (cape-company-to-capf #'company-yasnippet))
+                       result)
+        (add-to-list 'completion-at-point-functions element)))
+    )
+
+  ;; Eshell
+  (defun kb/cape-capf-setup-eshell ()
+    (let ((result))
+      (dolist (element '(pcomplete-completions-at-point cape-file) result)
+        (add-to-list 'completion-at-point-functions element))
+      ))
+
+  ;; LaTeX
+  ;; (defun kb/cape-capf-setup-latex ()
+  ;;   (require 'company-auctex)
+  ;;   (let ((result))
+  ;;     (dolist (element (list
+  ;;                       ;; First add `company-yasnippet'
+  ;;                       (cape-company-to-capf #'company-yasnippet)
+  ;;                       ;; Then add `cape-tex'
+  ;;                       #'cape-tex
+  ;;                       ;; Then add `company-auctex' in the order it adds its
+  ;;                       ;; backends.
+  ;;                       (cape-company-to-capf #'company-auctex-bibs)
+  ;;                       (cape-company-to-capf #'company-auctex-labels)
+  ;;                       (cape-company-to-capf
+  ;;                        (apply-partially #'company--multi-backend-adapter
+  ;;                                         '(company-auctex-macros company-auctex-symbols company-auctex-environments))))
+  ;;                      result)
+  ;;       (add-to-list 'completion-at-point-functions element))))
+
+
+  ;; Sh
+  (defun kb/cape-capf-setup-sh ()
+    (require 'company-shell)
+    (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-shell)))
+  :config
+  ;; For pcomplete. For now these two advices are strongly recommended to
+  ;; achieve a sane Eshell experience. See
+  ;; https://github.com/minad/corfu#completing-with-corfu-in-the-shell-or-eshell
+
+  ;; Silence the pcomplete capf, no errors or messages!
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+  ;; Ensure that pcomplete does not write to the buffer and behaves as a pure
+  ;; `completion-at-point-function'.
+  (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
+
 
 (use-package lsp-mode
   :config
@@ -359,209 +554,6 @@
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands (lsp lsp-deferred))
-
-;; (use-package corfu
-;;   :custom
-;;   (corfu-cycle t)
-;;   (corfu-auto t)
-;;   (corfu-auto-prefix 2)
-;;   (corfu-auto-delay 0.05)
-;;   (corfu-quit-no-match t)
-;;   (corfu-preview-current 'insert)
-;;   (corfu-preselect 'prompt)
-
-;;   (lsp-completion-provider :none) ; Use corfu instead for lsp completions
-
-;;   ;; Works with `indent-for-tab-command'. Make sure tab doesn't indent when you
-;;   ;; want to perform completion
-;;   (tab-always-indent 'complete)
-;;   (completion-cycle-threshold nil)    ; Always show candidates in menu
-  
-;;   :bind
-;;   (:map corfu-map
-;; 	("C-n"		.	'corfu-next)
-;; 	("C-p"		.	'corfu-previous)
-;; 	("<escape>"	.	'corfu-quit)
-;; 	("<return>"	.	'corfu-insert)
-;; 	("S-SPC"	.	'corfu-insert-separator))
-;;   :init
-;;   (global-corfu-mode)
-;;   (corfu-history-mode)
-;;   :config
-;;   (add-hook 'eshell-mode-hook
-;; 	    (lambda () (setq-local corfu-quit-at-boundary t
-;; 				   corfu-quit-no-match t
-;; 				   corfu-auto nil)
-;; 	      (corfu-mode)))
-;;   ;; Enable Corfu more generally for every minibuffer, as long as no other
-;;   ;; completion UI is active. If you use Mct or Vertico as your main minibuffer
-;;   ;; completion UI. From
-;;   ;; https://github.com/minad/corfu#completing-with-corfu-in-the-minibuffer
-;;   (defun corfu-enable-always-in-minibuffer ()
-;;     "Enable Corfu in the minibuffer if Vertico/Mct are not active."
-;;     (unless (or (bound-and-true-p mct--active) ; Useful if I ever use MCT
-;;                 (bound-and-true-p vertico--input))
-;;       (setq-local corfu-auto nil) ; Ensure auto completion is disabled
-;;       (corfu-mode 1)))
-;;   (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1)
-;;   ;; Setup lsp to use corfu for lsp completion
-;;   (defun kb/corfu-setup-lsp ()
-;;     "Use orderless completion style with lsp-capf instead of the default lsp-passthrough."
-;;     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-;;           '(orderless))))
-
-;; (use-package kind-icon
-;;   :after corfu
-;;   :custom
-;;   (kind-icon-use-icons t)
-;;   (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
-;;   (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
-;;   (kind-icon-blend-frac 0.08)
-
-;;   ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
-;;   ;; directory that defaults to the `user-emacs-directory'. Here, I change that
-;;   ;; directory to a location appropriate to `no-littering' conventions, a
-;;   ;; package which moves directories of other packages to sane locations.
-;;   ;; (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
-;;   :config
-;;   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
-
-;;   ;; Add hook to reset cache so the icon colors match my theme
-;;   ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
-;;   ;; the theme using my custom defined command for switching themes. If I don't
-;;   ;; do this, then the backgound color will remain the same, meaning it will not
-;;   ;; match the background color corresponding to the current theme. Important
-;;   ;; since I have a light theme and dark theme I switch between. This has no
-;;   ;; function unless you use something similar
-;;   (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
-
-;; (use-package cape
-;;   :hook ((emacs-lisp-mode .  kb/cape-capf-setup-elisp)
-;;          (lsp-completion-mode . kb/cape-capf-setup-lsp)
-;;          (org-mode . kb/cape-capf-setup-org)
-;;          (eshell-mode . kb/cape-capf-setup-eshell)
-;;          (git-commit-mode . kb/cape-capf-setup-git-commit)
-;;          (LaTeX-mode . kb/cape-capf-setup-latex)
-;;          (sh-mode . kb/cape-capf-setup-sh)
-;;          )
-;;   ;; :general (:prefix "M-c"               ; Particular completion function
-;;   ;;           "p" 'completion-at-point
-;;   ;;           "t" 'complete-tag           ; etags
-;;   ;;           "d" 'cape-dabbrev           ; or dabbrev-completion
-;;   ;;           "f" 'cape-file
-;;   ;;           "k" 'cape-keyword
-;;   ;;           "s" 'cape-symbol
-;;   ;;           "a" 'cape-abbrev
-;;   ;;           "i" 'cape-ispell
-;;   ;;           "l" 'cape-line
-;;   ;;           "w" 'cape-dict
-;;   ;;           "\\"' cape-tex
-;;   ;;           "_" 'cape-tex
-;;   ;;           "^" 'cape-tex
-;;   ;;           "&" 'cape-sgml
-;;   ;;           "r" 'cape-rfc1345
-;;   ;;           )
-;;   :custom
-;;   (cape-dabbrev-min-length 3)
-;;   :init
-;;   ;; Elisp
-;;   (defun kb/cape-capf-ignore-keywords-elisp (cand)
-;;     "Ignore keywords with forms that begin with \":\" (e.g.
-;; :history)."
-;;     (or (not (keywordp cand))
-;;         (eq (char-after (car completion-in-region--data)) ?:)))
-;;   (defun kb/cape-capf-setup-elisp ()
-;;     "Replace the default `elisp-completion-at-point'
-;; completion-at-point-function. Doing it this way will prevent
-;; disrupting the addition of other capfs (e.g. merely setting the
-;; variable entirely, or adding to list).
-
-;; Additionally, add `cape-file' as early as possible to the list."
-;;     (setf (elt (cl-member 'elisp-completion-at-point completion-at-point-functions) 0)
-;;           #'elisp-completion-at-point)
-;;     (add-to-list 'completion-at-point-functions #'cape-symbol)
-;;     ;; I prefer this being early/first in the list
-;;     (add-to-list 'completion-at-point-functions #'cape-file)
-;;     (require 'company-yasnippet)
-;;     (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-yasnippet)))
-
-;;   ;; LSP
-;;   (defun kb/cape-capf-setup-lsp ()
-;;     "Replace the default `lsp-completion-at-point' with its
-;; `cape-capf-buster' version. Also add `cape-file' and
-;; `company-yasnippet' backends."
-;;     (setf (elt (cl-member 'lsp-completion-at-point completion-at-point-functions) 0)
-;;           (cape-capf-buster #'lsp-completion-at-point))
-;;     ;; TODO 2022-02-28: Maybe use `cape-wrap-predicate' to have candidates
-;;     ;; listed when I want?
-;;     (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-yasnippet))
-;;     (add-to-list 'completion-at-point-functions #'cape-dabbrev t))
-
-;;   ;; Org
-;;   (defun kb/cape-capf-setup-org ()
-;;     ;; (require 'org-roam)
-;;     ;; (if (org-roam-file-p)
-;;     ;;     (org-roam--register-completion-functions-h)
-;;       (let (result)
-;;         (dolist (element (list
-;;                           (cape-super-capf #'cape-ispell #'cape-dabbrev)
-;;                           (cape-company-to-capf #'company-yasnippet))
-;;                          result)
-;;           (add-to-list 'completion-at-point-functions element)))
-;;       )
-
-;;   ;; Eshell
-;;   (defun kb/cape-capf-setup-eshell ()
-;;     (let ((result))
-;;       (dolist (element '(pcomplete-completions-at-point cape-file) result)
-;;         (add-to-list 'completion-at-point-functions element))
-;;       ))
-
-;;   ;; Git-commit
-;;   (defun kb/cape-capf-setup-git-commit ()
-;;     (general-define-key
-;;      :keymaps 'local
-;;      :states 'insert
-;;      "<tab>" 'completion-at-point)      ; Keybinding for `completion-at-point'
-;;     (let ((result))
-;;       (dolist (element '(cape-dabbrev cape-symbol) result)
-;;         (add-to-list 'completion-at-point-functions element))))
-
-;;   ;; LaTeX
-;;   (defun kb/cape-capf-setup-latex ()
-;;     (require 'company-auctex)
-;;     (let ((result))
-;;       (dolist (element (list
-;;                         ;; First add `company-yasnippet'
-;;                         (cape-company-to-capf #'company-yasnippet)
-;;                         ;; Then add `cape-tex'
-;;                         #'cape-tex
-;;                         ;; Then add `company-auctex' in the order it adds its
-;;                         ;; backends.
-;;                         (cape-company-to-capf #'company-auctex-bibs)
-;;                         (cape-company-to-capf #'company-auctex-labels)
-;;                         (cape-company-to-capf
-;;                          (apply-partially #'company--multi-backend-adapter
-;;                                           '(company-auctex-macros company-auctex-symbols company-auctex-environments))))
-;;                        result)
-;;         (add-to-list 'completion-at-point-functions element))))
-
-
-;;   ;; Sh
-;;   (defun kb/cape-capf-setup-sh ()
-;;     (require 'company-shell)
-;;     (add-to-list 'completion-at-point-functions (cape-company-to-capf #'company-shell)))
-;;   :config
-;;   ;; For pcomplete. For now these two advices are strongly recommended to
-;;   ;; achieve a sane Eshell experience. See
-;;   ;; https://github.com/minad/corfu#completing-with-corfu-in-the-shell-or-eshell
-
-;;   ;; Silence the pcomplete capf, no errors or messages!
-;;   (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
-;;   ;; Ensure that pcomplete does not write to the buffer and behaves as a pure
-;;   ;; `completion-at-point-function'.
-;;   (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify))
-
 
 (use-package which-key
   :diminish which-key-mode
