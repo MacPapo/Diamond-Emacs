246e0858ccdd6a14b5a086097d5f304eb214dbc4
MacPapo <jacopocostantini32@gmail.com>
2023-02-17 11:04:10 +0100

    Company mode + Lsp mode for fast coding

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(doc-view-continuous t)
  '(package-selected-packages
-   '(gcmh cape kind-icon tide treesit-auto js2-mode ag projectile-ag projectile-ripgrep company company-mode ruby-electric neotree web-mode mmm-mode projectile-rails projectile csv-mode docker explain-pause-mode grip-mode ess swift-mode haskell-mode suggest eshell-prompt-extras git-undo magit-todos forge org-bullets org-bullet sly lispy rspec-mode robe yari bundler move-dup smartparens phi-search multiple-cursors highlight-thing indent-guide dimmer solaire-mode ctrlf buffer-move flx-ido smex ido-vertical-mode flex-isearch ido-at-point ido-completing-read+ olivetti counsel org-pomodoro winum which-key dashboard ivy magit beacon auto-package-update corfu exec-path-from-shell diminish)))
+   '(yasnippet-snippets yasnippet lsp-mode gcmh cape kind-icon tide treesit-auto js2-mode ag projectile-ag projectile-ripgrep company company-mode ruby-electric neotree web-mode mmm-mode projectile-rails projectile csv-mode docker explain-pause-mode grip-mode ess swift-mode haskell-mode suggest eshell-prompt-extras git-undo magit-todos forge org-bullets org-bullet sly lispy rspec-mode robe yari bundler move-dup smartparens phi-search multiple-cursors highlight-thing indent-guide dimmer solaire-mode ctrlf buffer-move flx-ido smex ido-vertical-mode flex-isearch ido-at-point ido-completing-read+ olivetti counsel org-pomodoro winum which-key dashboard ivy magit beacon auto-package-update corfu exec-path-from-shell diminish)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -66,7 +66,6 @@
 (setq user-full-name "Jacopo Costantini")
 (setq user-mail-address "891938@stud.unive.it")
 
-(setq read-process-output-max (* 4 1024 1024))
 (setq global-prettify-symbols-mode 1)
 
 (set-charset-priority 'unicode)
@@ -205,10 +204,10 @@
 
 ;;; Eglot
 ;; (setq eglot-autoshutdown t)
-(add-hook 'c-ts-mode-hook 'eglot-ensure)
-(add-hook 'c++-ts-mode-hook 'eglot-ensure)
-(add-hook 'ruby-ts-mode-hook 'eglot-ensure)
-(add-hook 'java-ts-mode-hook 'eglot-ensure)
+;; (add-hook 'c-ts-mode-hook 'eglot-ensure)
+;; (add-hook 'c++-ts-mode-hook 'eglot-ensure)
+;; (add-hook 'ruby-ts-mode-hook 'eglot-ensure)
+;; (add-hook 'java-ts-mode-hook 'eglot-ensure)
 ;; (add-hook 'rust-mode-hook 'eglot-ensure)
 
 ;; setup files ending in .java to open in java-tree-sitter-mode
@@ -306,85 +305,135 @@
 ;;   :init
 ;;   (ido-at-point-mode))
 
-(use-package treesit-auto
-  :demand t
-  :config
-  (global-treesit-auto-mode))
+;; (use-package treesit-auto
+;;   :demand t
+;;   :config
+;;   (global-treesit-auto-mode))
 
-(use-package corfu
-  :custom
-  (corfu-cycle t)
-  (corfu-auto t)
-  (corfu-auto-prefix 2)
-  (corfu-auto-delay 0.05)
-  (corfu-quit-no-match t)
-  (corfu-preview-current 'insert)
-  (corfu-preselect 'prompt)
-
-  (lsp-completion-provider :none) ; Use corfu instead for lsp completions
-
-  ;; Works with `indent-for-tab-command'. Make sure tab doesn't indent when you
-  ;; want to perform completion
-  (tab-always-indent 'complete)
-  (completion-cycle-threshold nil)    ; Always show candidates in menu
-  
-  :bind
-  (:map corfu-map
-	("C-n"		.	'corfu-next)
-	("C-p"		.	'corfu-previous)
-	("<escape>"	.	'corfu-quit)
-	("<return>"	.	'corfu-insert)
-	("S-SPC"	.	'corfu-insert-separator))
+(use-package yasnippet
   :init
-  (global-corfu-mode)
-  (corfu-history-mode)
-  :config
-  (add-hook 'eshell-mode-hook
-	    (lambda () (setq-local corfu-quit-at-boundary t
-				   corfu-quit-no-match t
-				   corfu-auto nil)
-	      (corfu-mode)))
-  ;; Enable Corfu more generally for every minibuffer, as long as no other
-  ;; completion UI is active. If you use Mct or Vertico as your main minibuffer
-  ;; completion UI. From
-  ;; https://github.com/minad/corfu#completing-with-corfu-in-the-minibuffer
-  (defun corfu-enable-always-in-minibuffer ()
-    "Enable Corfu in the minibuffer if Vertico/Mct are not active."
-    (unless (or (bound-and-true-p mct--active) ; Useful if I ever use MCT
-                (bound-and-true-p vertico--input))
-      (setq-local corfu-auto nil) ; Ensure auto completion is disabled
-      (corfu-mode 1)))
-  (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1)
-  ;; Setup lsp to use corfu for lsp completion
-  (defun kb/corfu-setup-lsp ()
-    "Use orderless completion style with lsp-capf instead of the default lsp-passthrough."
-    (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
-          '(orderless))))
+  (yas-global-mode 1))
 
-(use-package kind-icon
-  :after corfu
+(use-package yasnippet-snippets
+  :after yasnippet)
+
+(use-package company
+  :diminish company-mode
   :custom
-  (kind-icon-use-icons t)
-  (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
-  (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
-  (kind-icon-blend-frac 0.08)
-
-  ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
-  ;; directory that defaults to the `user-emacs-directory'. Here, I change that
-  ;; directory to a location appropriate to `no-littering' conventions, a
-  ;; package which moves directories of other packages to sane locations.
-  ;; (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+  (company-idle-delay 0)
+  (company-minimum-prefix-length 1)
+  (company-selection-wrap-around t)
   :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
 
-  ;; Add hook to reset cache so the icon colors match my theme
-  ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
-  ;; the theme using my custom defined command for switching themes. If I don't
-  ;; do this, then the backgound color will remain the same, meaning it will not
-  ;; match the background color corresponding to the current theme. Important
-  ;; since I have a light theme and dark theme I switch between. This has no
-  ;; function unless you use something similar
-  (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
+  (defun my-text-mode ()
+    (setq-local company-backends
+		'((company-dabbrev company-ispell :separate)
+                  company-files)))
+
+  (defun my-prog-mode ()
+    (setq-local company-backends
+		'((company-capf :with company-yasnippet)
+                  (company-capf company-dabbrev-code))))
+
+  (setq company-backends '((company-capf :with company-yasnippet)))
+  :hook
+  ((text-mode . my-text-mode))
+  ((prog-mode . my-prog-mode))
+  :init
+  (global-company-mode))
+
+(use-package lsp-mode
+  :config
+  (setq read-process-output-max (* 3 1024 1024)) ; 3MB
+  :custom
+  (lsp-idle-delay 0.100)
+  (lsp-log-io nil)	  ; if set to true can cause a performance hit
+  :init
+  (require 'lsp-ido)
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+	 (ruby-mode . lsp-deferred)
+	 (c-mode . lsp-deferred)
+	 (c++-mode . lsp-deferred)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deferred))
+
+;; (use-package corfu
+;;   :custom
+;;   (corfu-cycle t)
+;;   (corfu-auto t)
+;;   (corfu-auto-prefix 2)
+;;   (corfu-auto-delay 0.05)
+;;   (corfu-quit-no-match t)
+;;   (corfu-preview-current 'insert)
+;;   (corfu-preselect 'prompt)
+
+;;   (lsp-completion-provider :none) ; Use corfu instead for lsp completions
+
+;;   ;; Works with `indent-for-tab-command'. Make sure tab doesn't indent when you
+;;   ;; want to perform completion
+;;   (tab-always-indent 'complete)
+;;   (completion-cycle-threshold nil)    ; Always show candidates in menu
+  
+;;   :bind
+;;   (:map corfu-map
+;; 	("C-n"		.	'corfu-next)
+;; 	("C-p"		.	'corfu-previous)
+;; 	("<escape>"	.	'corfu-quit)
+;; 	("<return>"	.	'corfu-insert)
+;; 	("S-SPC"	.	'corfu-insert-separator))
+;;   :init
+;;   (global-corfu-mode)
+;;   (corfu-history-mode)
+;;   :config
+;;   (add-hook 'eshell-mode-hook
+;; 	    (lambda () (setq-local corfu-quit-at-boundary t
+;; 				   corfu-quit-no-match t
+;; 				   corfu-auto nil)
+;; 	      (corfu-mode)))
+;;   ;; Enable Corfu more generally for every minibuffer, as long as no other
+;;   ;; completion UI is active. If you use Mct or Vertico as your main minibuffer
+;;   ;; completion UI. From
+;;   ;; https://github.com/minad/corfu#completing-with-corfu-in-the-minibuffer
+;;   (defun corfu-enable-always-in-minibuffer ()
+;;     "Enable Corfu in the minibuffer if Vertico/Mct are not active."
+;;     (unless (or (bound-and-true-p mct--active) ; Useful if I ever use MCT
+;;                 (bound-and-true-p vertico--input))
+;;       (setq-local corfu-auto nil) ; Ensure auto completion is disabled
+;;       (corfu-mode 1)))
+;;   (add-hook 'minibuffer-setup-hook #'corfu-enable-always-in-minibuffer 1)
+;;   ;; Setup lsp to use corfu for lsp completion
+;;   (defun kb/corfu-setup-lsp ()
+;;     "Use orderless completion style with lsp-capf instead of the default lsp-passthrough."
+;;     (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults))
+;;           '(orderless))))
+
+;; (use-package kind-icon
+;;   :after corfu
+;;   :custom
+;;   (kind-icon-use-icons t)
+;;   (kind-icon-default-face 'corfu-default) ; Have background color be the same as `corfu' face background
+;;   (kind-icon-blend-background nil)  ; Use midpoint color between foreground and background colors ("blended")?
+;;   (kind-icon-blend-frac 0.08)
+
+;;   ;; NOTE 2022-02-05: `kind-icon' depends `svg-lib' which creates a cache
+;;   ;; directory that defaults to the `user-emacs-directory'. Here, I change that
+;;   ;; directory to a location appropriate to `no-littering' conventions, a
+;;   ;; package which moves directories of other packages to sane locations.
+;;   ;; (svg-lib-icons-dir (no-littering-expand-var-file-name "svg-lib/cache/")) ; Change cache dir
+;;   :config
+;;   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter) ; Enable `kind-icon'
+
+;;   ;; Add hook to reset cache so the icon colors match my theme
+;;   ;; NOTE 2022-02-05: This is a hook which resets the cache whenever I switch
+;;   ;; the theme using my custom defined command for switching themes. If I don't
+;;   ;; do this, then the backgound color will remain the same, meaning it will not
+;;   ;; match the background color corresponding to the current theme. Important
+;;   ;; since I have a light theme and dark theme I switch between. This has no
+;;   ;; function unless you use something similar
+;;   (add-hook 'kb/themes-hooks #'(lambda () (interactive) (kind-icon-reset-cache))))
 
 ;; (use-package cape
 ;;   :hook ((emacs-lisp-mode .  kb/cape-capf-setup-elisp)
