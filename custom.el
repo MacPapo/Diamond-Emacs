c211d4c5fdf75a54ce0dbbccd6ff3f5aaaf821a4
Jacopo Costantini <macpapo@Jacopos-MBP.homenet.telecomitalia.it>
2023-03-02 23:49:55 +0100

    Added REST API IDE and some other tweaks

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(js2-refactor typescript-mode lsp-sourcekit browse-kill-ring flycheck-elm elm-mode csv-mode docker grip-mode ess swift-mode haskell-mode suggest sly ruby-electric rspec-mode yari bundler tide js2-mode web-mode ag projectile-ripgrep projectile-rails move-dup phi-search multiple-cursors indent-guide dimmer ctrlf buffer-move winum lsp-ui company-box lsp-java treemacs-magit treemacs-projectile treemacs company yasnippet-snippets yasnippet flycheck which-key magit-todos forge magit smex ido-completing-read+ beacon auto-package-update org-superstar org-pomodoro gcmh diminish use-package-ensure-system-package use-package exec-path-from-shell))
+   '(quelpa-use-package skewer-mode skewer simple-httpd httpcode httcode httprepl restclient js2-refactor typescript-mode lsp-sourcekit browse-kill-ring flycheck-elm elm-mode csv-mode docker grip-mode ess swift-mode haskell-mode suggest sly ruby-electric rspec-mode yari bundler tide js2-mode web-mode ag projectile-ripgrep projectile-rails move-dup phi-search multiple-cursors indent-guide dimmer ctrlf buffer-move winum lsp-ui company-box lsp-java treemacs-magit treemacs-projectile treemacs company yasnippet-snippets yasnippet flycheck which-key magit-todos forge magit smex ido-completing-read+ beacon auto-package-update org-superstar org-pomodoro gcmh diminish use-package-ensure-system-package use-package exec-path-from-shell))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
